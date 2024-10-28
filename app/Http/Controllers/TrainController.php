168417ef08edecb633cf6ea2a2cc04893485c17f
Marco-Guarnera <Darkaquon93@gmail.com>
2024-10-28 08:17:50 +0100

    Recovered data from database

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function homepage() {
+        $trains_list = Train::all();
+        @dd($trains_list);
+        return view('pages.home-page', compact('trains_list'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home-page');
-})->name('homepage');
+// Homepage
+Route::get('/', [TrainController::class, 'homepage'])->name('homepage');
