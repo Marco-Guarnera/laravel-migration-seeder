a9a8b29c95c772aa66008389f6f0a116cda611ed
Marco-Guarnera <Darkaquon93@gmail.com>
2024-10-29 09:24:03 +0100

    Updated TrainSeeder.php

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 10; $i++) {
+            $train = new Train();
+            $train->azienda = $faker->company();
+            $train->stazione_partenza = $faker->city();
+            $train->stazione_arrivo = $faker->city();
+            $train->data_partenza = $faker->dateTimeBetween('now', '+1 week');
+            $train->data_arrivo = $faker->dateTimeBetween('now', '+1 week');
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->codice_treno = $faker->regexify('[A-Z]{2}[0-9]{4}');
+            $train->numero_carrozze = $faker->numberBetween(1, 25);
+            $train->in_orario = $faker->boolean();
+            $train->cancellato = $faker->boolean();
+            $train->save();
+        }
     }
 }
